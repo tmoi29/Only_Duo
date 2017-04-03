acfec82a514c3294015e85dff258dbd5ac6518b8
Brandontzhang <zhangtbrandon2@gmail.com>
2017-04-03 19:01:53 -0400

    size() changes

--- TheOnlyDuo.java
+++ TheOnlyDuo.java
@@ -3,13 +3,11 @@ import java.util.ArrayList;
 public class TheOnlyDuo<E> implements Deque<E>{
 
     //Instance vars
-    private int _size;
     private ArrayList<E> _deque;
 
     //Default constructor
     public TheOnlyDuo(){
 	_deque = new ArrayList<E>();
-	_size = 0;
     }
 
     //Adds element to front
@@ -19,20 +17,18 @@ public class TheOnlyDuo<E> implements Deque<E>{
 
     //Adds element to back
     public void addLast(E x){
-	_deque.add(_size,x);
+	_deque.add(_deque.size(),x);
     }
 
     //Removes element from front
     public E removeFirst(){
 	E x = _deque.remove(0);
-	_size--;
 	return x;
     }
 
     //Removes element from back
     public E removeLast(){
-	E x = _deque.remove(_size-1);
-	_size--;
+	E x = _deque.remove(_deque.size()-1);
 	return x;
     }
 
@@ -44,7 +40,7 @@ public class TheOnlyDuo<E> implements Deque<E>{
 
     //Returns element at back
     public E peekLast(){
-	return _deque.get(_size-1);
+	return _deque.get(_deque.size()-1);
     }
 
     //Returns true/false whether queue is empty or not
@@ -52,13 +48,12 @@ public class TheOnlyDuo<E> implements Deque<E>{
 	return _deque.get(0) == null;
     }
 
-    //Returns number of meaningful elements in queue
     public int size(){
-	return _size;
+	return _deque.size();
     }
+
 
     //For testing purposes
     public static void main(String[] args){
-
     }
 }
