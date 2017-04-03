8ff28e0bd954e7a455c92343f540db642fa8f2c2
Jason Lam <jlam6@stuy.edu>
2017-04-03 11:46:01 -0400

    continued implementation

--- TheOnlyDuo.java
+++ TheOnlyDuo.java
@@ -12,46 +12,47 @@ public class TheOnlyDuo<T> implements Deque<T>{
 	_size = 0;
     }
 
-    //
+    //Adds element to front
     public void addFirst(T x){
-	_deque.add(x);
+	_deque.add(0,x);
     }
 
-    //
+    //Adds element to back
     public void addLast(T x){
 	_deque.add(_size,x);
     }
 
-    //
+    //Removes element from front
     public T removeFirst(){
 	T x = _deque.remove(0);
 	_size--;
 	return x;
     }
 
-    //
+    //Removes element from back
     public T removeLast(){
 	T x = _deque.remove(_size-1);
 	_size--;
 	return x;
     }
 
-    //
+    //Returns element at front
     public T peekFirst(){
         return _deque.get(0);
 
     }
 
-    //
+    //Returns element at back
     public T peekLast(){
 	return _deque.get(_size-1);
     }
 
-    //
+    //Returns true/false whether queue is empty or not
     public boolean isEmpty(){
 	return _deque.get(0) == null;
     }
 
+    //Returns number of meaningful elements in queue
     public int size(){
 	return _size;
     }
